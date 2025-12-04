e9142522e4545f0309612d2bf82f06c5074650e6
Karthik9800 <karthikeya.t@dhatvibs.com>
2025-12-04 12:19:42 +0530

    Small Change in Aadhar Screen

--- src/screens/onboarding/AadharVerifyScreen.js
+++ src/screens/onboarding/AadharVerifyScreen.js
@@ -1,14 +1,14 @@
 import { View, Text ,TouchableOpacity} from 'react-native'
 import React from 'react'
-import documentVerifyScreen from './documentVerifyScreen'
+import DocumentVerifyScreen from './DocumentVerifyScreen'
 
-const aadharVerifyScreen = ({navigation}) => {
+const AadharVerifyScreen = ({navigation}) => {
   return (
     <View style={{flex:1,backgroundColor:"black"}}>
           
           <View style={{margin:70}}>
           <Text style={{color:'white'}}>aadharV</Text>
-          <TouchableOpacity onPress={()=>navigation.navigate(documentVerifyScreen)}>
+          <TouchableOpacity onPress={()=>navigation.navigate(DocumentVerifyScreen)}>
             <Text style={{color:"white"}}>Next</Text>
           </TouchableOpacity>
         
@@ -16,4 +16,4 @@ const aadharVerifyScreen = ({navigation}) => {
   )
 }
 
-export default aadharVerifyScreen
+export default AadharVerifyScreen
