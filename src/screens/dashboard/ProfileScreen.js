54fdd68852a7878c6974cbdf588783e015382d6a
bhargaviT12 <bhargavi.t@dhatvibs.com>
2026-01-12 17:24:57 +0530

    bha

--- src/screens/dashboard/ProfileScreen.js
+++ src/screens/dashboard/ProfileScreen.js
@@ -244,10 +244,15 @@ export default function ProfileScreen({ navigation }) {
                   </Text>
                 </View>
               </View>
+ 
+             
               <Text style={styles.arrow}>›</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.listItemReduced} activeOpacity={0.7}>
+            <TouchableOpacity
+              style={styles.listItemReduced}
+              activeOpacity={0.7}
+            >
               <View style={styles.listLeft}>
                 <Image
                   source={require('../../assets/profile/Bank.png')}
@@ -284,10 +289,7 @@ export default function ProfileScreen({ navigation }) {
           {/* PERFORMANCE & REWARDS */}
           <View style={styles.sectionContainer}>
             <Text style={styles.sectionTitle}>PERFORMANCE & REWARDS</Text>
-            <TouchableOpacity
-              style={styles.listItemReduced}
-              activeOpacity={0.7}
-            >
+            <TouchableOpacity style={styles.listItemReduced} activeOpacity={0.7} onPress={() => navigation.navigate("RewardsScreen")}>
               <View style={styles.listLeft}>
                 <Image
                   source={require('../../assets/profile/Rewards.png')}
@@ -307,7 +309,13 @@ export default function ProfileScreen({ navigation }) {
           {/* HISTORY & ACTIVITY */}
           <View style={styles.sectionContainer}>
             <Text style={styles.sectionTitle}>HISTORY & ACTIVITY</Text>
-            <TouchableOpacity style={styles.listItemReduced} activeOpacity={0.7}>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('OrderHistory');
+              }}
+              style={styles.listItemReduced}
+              activeOpacity={0.7}
+            >
               <View style={styles.listLeft}>
                 <Image
                   source={require('../../assets/profile/Order.png')}
@@ -322,7 +330,10 @@ export default function ProfileScreen({ navigation }) {
               </View>
               <Text style={styles.arrow}>›</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.listItemReduced} activeOpacity={0.7}>
+            <TouchableOpacity
+              style={styles.listItemReduced}
+              activeOpacity={0.7}
+            >
               <View style={styles.listLeft}>
                 <Image
                   source={require('../../assets/profile/Slot.png')}
@@ -476,9 +487,9 @@ const styles = StyleSheet.create({
   },
 
   headerTitle: {
-    color: '#FFFFFF',
-    fontSize: wp('6%'),
-    fontWeight: '600',
+    color: "#FFFFFF",
+    fontSize: wp("6.4%"),
+    fontWeight: "600",
   },
 
   profileCard: {
@@ -512,15 +523,15 @@ const styles = StyleSheet.create({
   },
 
   name: {
-    fontSize: wp('4.5%'),
-    fontWeight: '600',
-    color: '#222',
+    fontSize: wp("4.8%"),
+    fontWeight: "600",
+    color: "#222",
   },
 
   partnerId: {
-    fontSize: wp('3.2%'),
-    color: '#777',
-    marginTop: hp('0.3%'),
+    fontSize: wp("3.5%"),
+    color: "#777",
+    marginTop: hp("0.3%"),
   },
 
   activeBadge: {
@@ -533,9 +544,9 @@ const styles = StyleSheet.create({
   },
 
   activeText: {
-    fontSize: wp('3%'),
-    color: '#2E7D32',
-    fontWeight: '500',
+    fontSize: wp("3.3%"),
+    color: "#2E7D32",
+    fontWeight: "500",
   },
 
   divider: {
@@ -555,15 +566,15 @@ const styles = StyleSheet.create({
   },
 
   statValue: {
-    fontSize: wp('4.5%'),
-    fontWeight: '600',
-    color: '#222',
+    fontSize: wp("4.9%"),
+    fontWeight: "600",
+    color: "#222",
   },
 
   statLabel: {
-    fontSize: wp('3%'),
-    color: '#777',
-    marginTop: hp('0.5%'),
+    fontSize: wp("3.3%"),
+    color: "#777",
+    marginTop: hp("0.5%"),
   },
 
   verticalDivider: {
@@ -578,13 +589,13 @@ const styles = StyleSheet.create({
   },
 
   sectionTitle: {
-    fontWeight: '400',
-    fontSize: wp('3.4%'),
-    lineHeight: hp('2.5%'),
-    letterSpacing: 0.35,
-    textTransform: 'uppercase',
-    color: '#9AA0A6',
-    marginBottom: hp('1%'),
+    fontWeight: "400",
+    fontSize: wp("3.7%"),
+    lineHeight: hp("2.7%"),
+    letterSpacing: 0.4,
+    textTransform: "uppercase",
+    color: "#9AA0A6",
+    marginBottom: hp("1%"),
   },
 
   listItemReduced: {
@@ -603,20 +614,20 @@ const styles = StyleSheet.create({
   },
 
   listTitle: {
-    fontSize: wp('4%'),
-    fontWeight: '500',
-    color: '#222',
+    fontSize: wp("4.3%"),
+    fontWeight: "500",
+    color: "#222",
   },
 
   listSubtitle: {
-    fontSize: wp('3.2%'),
-    color: '#777',
-    marginTop: hp('0.3%'),
+    fontSize: wp("3.5%"),
+    color: "#777",
+    marginTop: hp("0.3%"),
   },
 
   arrow: {
-    fontSize: wp('5%'),
-    color: '#9AA0A6',
+    fontSize: wp("5.4%"),
+    color: "#9AA0A6",
   },
 
   logoutButton: {
@@ -636,9 +647,9 @@ const styles = StyleSheet.create({
   },
 
   logoutText: {
-    color: '#FFFFFF',
-    fontSize: wp('4%'),
-    fontWeight: '600',
+    color: "#FFFFFF",
+    fontSize: wp("4.3%"),
+    fontWeight: "600",
   },
 
   logoutIcon: {
