a49bf0b33a98329057e07e4c02c73e244a1368d9
bhargaviT12 <bhargavi.t@dhatvibs.com>
2026-01-29 15:11:19 +0530

    initial route name

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -47,7 +47,7 @@ const AppNavigator = () => {
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false, statusBarStyle: 'light' }}
-      initialRouteName="MainTabs"
+      initialRouteName="SplashScreen"
     >
       <Stack.Screen name="KitPickupSelection" component={KitPickupSelection} />
       <Stack.Screen name="KitSelectionScreen" component={KitSelectionScreen} />
