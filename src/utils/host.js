3c70cb0546c5b7acb68f41439b85277acb71e5b1
himabindu <himabindu.k@dhatvibs.com>
2026-02-25 11:00:21 +0530

    added web socket url

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,3 +1,7 @@
 const WEBSITE_URL="https://delivarypartner.onrender.com";
 export default WEBSITE_URL;
 
+// const WEBSOCKET_URL="delivarypartner.onrender.com";
+// export default WEBSOCKET_URL;
+
+export const WEBSOCKET_URL = "wss://delivarypartner.onrender.com";
