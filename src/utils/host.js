b0478c0c1039aad0e3465494977dbecd910b58b9
Mahendra007 <mahendra.s@dhatvibs.com>
2026-03-03 12:02:22 +0530

    host

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,5 +1,4 @@
-//const WEBSITE_URL="https://delivarypartner.onrender.com";
-const WEBSITE_URL="https://rider-module.onrender.com";
+const WEBSITE_URL="https://delivarypartner.onrender.com";
 
 
 export default WEBSITE_URL;
@@ -7,5 +6,4 @@ export default WEBSITE_URL;
 // const WEBSOCKET_URL="delivarypartner.onrender.com";
 // export default WEBSOCKET_URL;
 
-//export const WEBSOCKET_URL = "wss://delivarypartner.onrender.com";
-export const WEBSOCKET_URL = "wss://rider-module.onrender.com";
+export const WEBSOCKET_URL = "wss://delivarypartner.onrender.com";
