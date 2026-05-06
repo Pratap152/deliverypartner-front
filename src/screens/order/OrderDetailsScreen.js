5a0abb35979795031804342cf486fee882c53774
Sri Poshma <sriposhma.y@dhatvibs.com>
2026-05-06 22:24:11 +0530

    feat: resolved import issue in order details screen

--- src/screens/order/OrderDetailsScreen.js
+++ src/screens/order/OrderDetailsScreen.js
@@ -14,6 +14,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { BackHandler } from 'react-native';
 import Geolocation from "@react-native-community/geolocation";
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { orderUIConfig } from '../../config/orderUIConfig';
 import {
   widthPercentageToDP as wp,
