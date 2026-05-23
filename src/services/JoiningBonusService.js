f6626f844a0ca13c76cf6ff353c1da64d3257ba2
kunasudharani <sudharani.k@dhatvibs.com>
2026-05-23 16:03:08 +0530

    integrated joining bonus flow

--- src/services/JoiningBonusService.js
+++ src/services/JoiningBonusService.js
@@ -1,25 +1,57 @@
+
 import apiClient from '../services/ApiClient';
 
-export const getJoiningBonusProgress = async () => {
+/* GET PROGRAMS */
+export const getReferralPrograms = async () => {
   const res = await apiClient.get(
-    '/api/refer/referral/referee-progress'
+    '/api/referral/programs'
   );
 
   return res.data;
 };
 
+/* GET PROGRAM PROGRESS */
+export const getReferralProgramsProgress = async () => {
+  const res = await apiClient.get(
+    '/api/referral/programs/progress'
+  );
+
+  return res.data;
+};
+
+/* TASK DESCRIPTION */
 export const getTaskDescription = (task) => {
-  if (!task?.slabs?.length) {
-    return 'Complete daily target';
+
+  switch (task.taskRuleType) {
+
+    case 'SLAB':
+      return (
+        task.slabs
+          ?.map((slab) => {
+            if (slab.maxOrders) {
+              return `${slab.minOrders}-${slab.maxOrders} orders → ₹${slab.rewardAmount}`;
+            }
+
+            return `${slab.minOrders}+ orders → ₹${slab.rewardAmount}`;
+          })
+          .join('\n') || 'Complete slab targets'
+      );
+
+    case 'FIXED_TARGET':
+      return `Complete ${task.target?.orders || 0} orders and earn ₹${task.reward?.amount || 0}`;
+
+    case 'PER_ORDER':
+      return `Earn ₹${task.rewardPerOrder || 0} per order upto ₹${task.maxEarning || 0}`;
+
+    case 'HYBRID':
+      return `
+Min Orders: ${task.conditions?.minOrders || 0}
+Acceptance Rate: ${task.conditions?.minAcceptanceRate || 0}%
+Completion Rate: ${task.conditions?.minCompletionRate || 0}%
+Min Earnings: ₹${task.conditions?.minEarnings || 0}
+      `;
+
+    default:
+      return 'Complete daily target';
   }
-
-  return task.slabs
-    .map((slab) => {
-      if (slab.maxOrders) {
-        return `${slab.minOrders}-${slab.maxOrders} orders → ₹${slab.rewardAmount}`;
-      }
-
-      return `${slab.minOrders}+ orders → ₹${slab.rewardAmount}`;
-    })
-    .join('\n');
 };
