69da06d903c900b79ff0cbb02db511c529ac1457
kunasudharani <sudharani.k@dhatvibs.com>
2026-07-16 18:12:57 +0530

    updated host with swach

--- src/utils/host.js
+++ src/utils/host.js
@@ -6,4 +6,4 @@ export default WEBSITE_URL;
 // const WEBSOCKET_URL="delivarypartner.onrender.com";
 // export default WEBSOCKET_URL;
 
-export const WEBSOCKET_URL = "wss://rider-module.onrender.com";
+export const WEBSOCKET_URL = "wss://rider-api.swachvega.com";
