af08fee3dbb632b7be0de01d0c1faca9a3882918
srikesh03 <srikeshreddy.g@dhatvibs.com>
2026-07-23 15:11:10 +0530

    gps

--- src/services/paymentsService.js
+++ src/services/paymentsService.js
@@ -2,14 +2,14 @@ import apiClient from './ApiClient';
 
 export const patchPayment = async (requestIdsForComplete) => {
     const response = await apiClient.patch(
-        `/api/kit/payments/complete?requestIds=${requestIdsForComplete}`
+        `/api/rider/kit/payments/complete?requestIds=${requestIdsForComplete}`
     );
     return response;
 };
 
 export const saveResponse = async (requestIds, paymentSelectionPayload) => {
     const response = await apiClient.post(
-        `/api/kit/payment?requestIds=${requestIds}`,
+        `/api/rider/kit/payment?requestIds=${requestIds}`,
         paymentSelectionPayload
     );
     return response;
