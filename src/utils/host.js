632ae15d11d2a33eca248bb408ed4569c8566941
Craftman <craftsman@dhatvibs.com>
2026-08-22 17:22:48 +0530

    Add WebSocket URL for rider module

--- src/utils/host.js
+++ src/utils/host.js
@@ -5,5 +5,6 @@ export default WEBSITE_URL;
 
 // const WEBSOCKET_URL="delivarypartner.onrender.com";
 // export default WEBSOCKET_URL;
+// https://rider-api.swachvega.com/ for dev
 
 export const WEBSOCKET_URL = "wss://rider-module.onrender.com";
